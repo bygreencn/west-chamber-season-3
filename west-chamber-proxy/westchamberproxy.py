3f5d4baedab93bb1066bf7fa83521d3429aae70b
liruqi <liruqi@gmail.com>
2012-03-07 17:45:21 +0800

    speed up

--- west-chamber-proxy/westchamberproxy.py
+++ west-chamber-proxy/westchamberproxy.py
@@ -177,6 +177,10 @@ class ProxyHandler(BaseHTTPRequestHandler):
                             continue 
                 if not found:
                     break
+
+            if (host in gConfig["HSTS_DOMAINS"]):
+                redirectUrl = "https://" + self.path[7:]
+
             #redirect 
             if (redirectUrl != self.path):
                 status = "HTTP/1.1 302 Found"
@@ -256,8 +260,6 @@ class ProxyHandler(BaseHTTPRequestHandler):
             if (netloc != urlparse.urlparse( gConfig["PROXY_SERVER"] )[1]):
                 self.wfile.write(status + "\r\n")
                 redirectUrl = gConfig["PROXY_SERVER"] + self.path[7:]
-                if (host in gConfig["HSTS_DOMAINS"]):
-                    redirectUrl = "https://" + self.path[7:]
                 self.wfile.write("Location: " + redirectUrl + "\r\n")
             else:
                 status = "HTTP/1.1 302 Found"
